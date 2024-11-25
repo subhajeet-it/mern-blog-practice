dbbf3b960fb5df6e0dca9c1e8a53490f24b4b7dc
subhajeet-it <subhajeet.eie@gmail.com>
2024-11-25 17:26:13 +0530

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "./components/Header";
 import { Outlet } from 'react-router-dom';
+import FooterCom from './components/Footer';
 const App = () => {
   return (
    <>
    <Header/>
    <Outlet/>
+   <FooterCom/>
    </>
   )
 }
