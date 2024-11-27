6eadb2cf5e09e7bd3aeed934d7c816d0ccbca8e4
subhajeet-it <subhajeet.eie@gmail.com>
2024-11-27 15:56:26 +0530

    add admin fucntionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,7 +38,7 @@ export const signin=async(req,res,next)=>{
            return  next(errorHandler(400,"Invalid credentials"))
         }
         
-        const token=jwt.sign({id:user._id},process.env.ACCESS_SECRET)
+        const token=jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.ACCESS_SECRET)
         const {password:pass,...rest}=user._doc;
             res.status(200).cookie("accessToken",token,{httpOnly:true}).json(rest);
     }catch(error){
@@ -51,7 +51,7 @@ export const google=async(req,res,next)=>{
     try{
         const user=await User.findOne({email});
         if(user){
-            const token=jwt.sign({id:user._id},process.env.ACCESS_SECRET)
+            const token=jwt.sign({id:user._id,isAdmin:user.isAdmin},process.env.ACCESS_SECRET)
             const {password:pass,...rest}=user._doc;
             res.status(200).cookie("accessToken",token,{httpOnly:true}).json(rest);
         }else{
@@ -64,7 +64,7 @@ export const google=async(req,res,next)=>{
                 profilePicture: photoURL
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.ACCESS_SECRET);
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.ACCESS_SECRET);
             const { password, ...rest } = newUser._doc;
             res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
         }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,6 +4,10 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profilePicture: { type: String, default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+      },
 })
 
 const User = mongoose.model("User", userSchema);
